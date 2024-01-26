9262607bc32dc1497e3c1bb7f7d8a14a79f7446a
Puvistyne Rajasegar <puvistien@gmail.com>
2024-01-26 16:08:58 +0100

    theme photos added

--- src/components/GallerySection.tsx
+++ src/components/GallerySection.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 
-import photo1 from '../assets/photos/1578103785-IMG-20230714-WA0015.jpg';
-import photo2 from '../assets/photos/1213748526-IMG-20230201-WA0001.jpg';
+import theme1 from '../assets/photos/theme1.jpeg';
+import theme1photo from '../assets/photos/theme1photo.jpeg';
 import photo3 from '../assets/photos/20231210_001947.jpg';
 import photo4 from '../assets/photos/IMG-20231224-WA0012.jpg';
 
@@ -10,20 +10,20 @@ const GallerySection = () => {
         <div className="container">
             <div className=" d-flex flex-row align-items-stretch ">
                 <div className="col-md-6 p-2">
-                    <img className=" rounded-3 img-fluid" src={photo1} alt={""}/>
+                    <img className=" rounded-3 img-fluid" src={theme1} alt={""}/>
                 </div>
                 <div className="col-md-6 p-2">
-                    <img className=" rounded-3 img-fluid" src={photo2} alt={""}/>
+                    <img className=" rounded-3 img-fluid" src={theme1photo} alt={""}/>
                 </div>
             </div>
-            <div className="row">
-                <div className="col-md-6 p-2">
-                    <img className=" rounded-3 img-fluid" src={photo3} alt={""}/>
-                </div>
-                <div className="col-md-6 p-2">
-                    <img className=" rounded-3 img-fluid" src={photo4} alt={""}/>
-                </div>
-            </div>
+            {/*<div className="row">*/}
+            {/*    <div className="col-md-6 p-2">*/}
+            {/*        <img className=" rounded-3 img-fluid" src={photo3} alt={""}/>*/}
+            {/*    </div>*/}
+            {/*    <div className="col-md-6 p-2">*/}
+            {/*        <img className=" rounded-3 img-fluid" src={photo4} alt={""}/>*/}
+            {/*    </div>*/}
+            {/*</div>*/}
         </div>
     );
 };
