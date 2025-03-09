c7deca70053a92108668f6d261aa0c745c0bf6d6
puvistyne <puvistien@gmail.com>
2025-03-09 20:47:33 +0100

    last i think

--- src/components/PhotoSection.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export default function PhotoSection() {
-    return (
-        <div className="photo-section">
-            <h1>Photo Section</h1>
-        </div>
-    );
-}
--- src/react-app-env.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference types="react-scripts" />
-declare module '*.mp3'; // '*.wav' if you're using wav format
-declare module '*.mp4'; // '*.wav' if you're using wav format
-declare module '*.mov'; // '*.wav' if you're using wav format
